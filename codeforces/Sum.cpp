924e4c23c67e9a59af3b3602f8a3d56e28a21c8b
Ahmed Essam <ahmedessamjax@gmail.com>
2026-08-15 16:42:30 +0300

    add codeforces/Sum solution

---
+++ codeforces/Sum.cpp
@@ -0,0 +1,39 @@
+// problem name: Sum
+// problem link: https://codeforces.com/problemset/problem/1742/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(i, a, b) for (int i = a; i < b; i++)
+#define ringr(i, a, b) for (int i = a; i > b; i--)
+#define rings(i, a, b, step) for (int i = a; i < b; i += step)
+#define sz(n) (n).size()
+#define ln cout << '\n'
+#define clr(mem, i) memset(mem, i, sizeof(mem))
+
+void solve() {
+  int a, b, c; cin >> a >> b >> c;
+  if(a + b == c || a + c == b || b + c == a) cout << "YES\n";
+  else cout << "NO\n";
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
