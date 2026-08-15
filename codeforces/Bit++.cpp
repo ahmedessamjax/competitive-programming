94b29c457a43b6d5ba2151f10ba7fd928e17a9bb
Ahmed Essam <ahmedessamjax@gmail.com>
2026-08-15 13:54:56 +0300

    add codeforces/Bit++ solution

---
+++ codeforces/Bit++.cpp
@@ -0,0 +1,44 @@
+// problem name: Bit++
+// problem link: https://codeforces.com/problemset/problem/282/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(i, a, b) for (int i = a; i < b; i++)
+#define ringr(i, a, b) for (int i = a; i > b; i--)
+#define rings(i, a, b, step) for (int i = a; i < b; i += step)
+#define sz(n) (n).size()
+#define ln cout << '\n'
+#define clr(mem, i) memset(mem, i, sizeof(mem))
+
+void solve() {
+	int t; cin >> t;
+	int cnt = 0;
+	while(t--){
+		string s; cin >> s;
+		if(s[1] == '+') cnt++;
+		else cnt--;
+	}
+	cout << cnt << '\n';
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t = 1;
+  // cin >> t;
+  while (t--) {
+    solve();
+  }
+}
