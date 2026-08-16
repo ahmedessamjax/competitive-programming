a5652bb114f041287f6cab1b4478484006fd456e
Ahmed Essam <ahmedessamjax@gmail.com>
2026-08-16 16:20:08 +0300

    add codeforces/Theatre Square solution

---
+++ codeforces/Theatre Square.cpp
@@ -0,0 +1,40 @@
+// problem name: Theatre Square
+// problem link: https://codeforces.com/contest/1/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(i, a, b) for (int i = a; i < b; i++)
+#define ringr(i, a, b) for (int i = a; i > b; i--)
+#define rings(i, a, b, step) for (int i = a; i < b; i += step)
+#define sz(n) (n).size()
+#define ln cout << '\n'
+#define clr(mem, i) memset(mem, i, sizeof(mem))
+
+void solve() {
+  long long  n, m;
+  double a;
+  cin >> n >> m >> a;
+  printf("%.0lf\n", ceil(n / a) * ceil(m / a)); 
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t = 1;
+  // cin >> t;
+  while (t--) {
+    solve();
+  }
+}
