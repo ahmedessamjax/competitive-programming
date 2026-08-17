556a2a27d2471766e5cc3b724a4290e11fe412fc
Ahmed Essam <ahmedessamjax@gmail.com>
2026-08-17 14:46:31 +0300

    add codeforces/Calculating Function solution

---
+++ codeforces/Calculating Function.cpp
@@ -0,0 +1,42 @@
+// problem name: Calculating Function
+// problem link: https://codeforces.com/problemset/problem/486/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(i, a, b) for (int i = a; i < b; i++)
+#define ringr(i, a, b) for (int i = a; i > b; i--)
+#define rings(i, a, b, step) for (int i = a; i < b; i += step)
+#define sz(n) (n).size()
+#define ln cout << '\n'
+#define clr(mem, i) memset(mem, i, sizeof(mem))
+
+void solve() {
+  ll n;
+  cin >> n;
+  ll oddN = (n + 1) / 2, evenN = n / 2;
+  ll odd = oddN * oddN;
+  ll even = evenN * (evenN + 1);
+  cout << even - odd << '\n';
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t = 1;
+  // cin >> t;
+  while (t--) {
+    solve();
+  }
+}
