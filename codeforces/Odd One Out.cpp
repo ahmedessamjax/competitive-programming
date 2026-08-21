fc61e13b13d7e65a055895412b405224c6de6419
Ahmed Essam <ahmedessamjax@gmail.com>
2026-08-21 17:50:07 +0300

    add codeforces/Odd One Out solution

---
+++ codeforces/Odd One Out.cpp
@@ -0,0 +1,41 @@
+// problem name: Odd One Out
+// problem link: https://codeforces.com/problemset/problem/1915/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(i, a, b) for (int i = a; i < b; i++)
+#define ringr(i, a, b) for (int i = a; i > b; i--)
+#define rings(i, a, b, step) for (int i = a; i < b; i += step)
+#define sz(n) (n).size()
+#define ln cout << '\n'
+#define clr(mem, i) memset(mem, i, sizeof(mem))
+
+void solve() {
+  int a, b, c; cin >> a >> b >> c ;
+  if(a == b) cout << c;
+  else if (a == c ) cout << b;
+  else cout << a;
+  cout << '\n';
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
