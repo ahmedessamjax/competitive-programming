0880c4819d18c7fa458e43c0ebf6186b4077b6e2
Ahmed Essam <ahmedessamjax@gmail.com>
2026-08-21 17:56:40 +0300

    add codeforces/Only One Digit solution

---
+++ codeforces/Only One Digit.cpp
@@ -0,0 +1,44 @@
+// problem name: Only One Digit
+// problem link: https://codeforces.com/problemset/problem/2126/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(i, a, b) for (int i = a; i < b; i++)
+#define ringr(i, a, b) for (int i = a; i > b; i--)
+#define rings(i, a, b, step) for (int i = a; i < b; i += step)
+#define sz(n) (n).size()
+#define ln cout << '\n'
+#define clr(mem, i) memset(mem, i, sizeof(mem))
+
+void solve() {
+  string s; cin >> s;
+  char mn = '9';
+  for(char c: s){
+    if( c < mn){
+      mn = c;
+    }
+  }
+  cout << mn << '\n';
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
