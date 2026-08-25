35161b979477745238b9bfc4a230c54bbeb03a20
Ahmed Essam <ahmedessamjax@gmail.com>
2026-08-25 22:30:03 +0300

    add codeforces/Vanya and Fence solution

---
+++ codeforces/Vanya and Fence.cpp
@@ -0,0 +1,44 @@
+// problem name: Vanya and Fence
+// problem link: https://codeforces.com/problemset/problem/677/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(i, a, b) for (int i = a; i < b; i++)
+#define ringr(i, a, b) for (int i = a; i > b; i--)
+#define rings(i, a, b, step) for (int i = a; i < b; i += step)
+#define sz(n) (n).size()
+#define ln cout << '\n'
+#define clr(mem, i) memset(mem, i, sizeof(mem))
+
+void solve() {
+  int n, h; cin >> n >> h;
+  int cnt =0;
+  ring(a, 0, n){
+    int x; cin >> x;
+    if(x > h) cnt+=2;
+    else cnt+=1;
+  }
+  cout << cnt << '\n';
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t = 1;
+  // cin >> t;
+  while (t--) {
+    solve();
+  }
+}
