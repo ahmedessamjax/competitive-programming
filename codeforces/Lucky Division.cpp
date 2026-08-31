af002e90ae36f6d201a208c2af09265e891f896c
Ahmed Essam <ahmedessamjax@gmail.com>
2026-08-31 21:09:47 +0300

    add codeforces/Lucky Division solution

---
+++ codeforces/Lucky Division.cpp
@@ -0,0 +1,45 @@
+// problem name: Lucky Division
+// problem link: https://codeforces.com/problemset/problem/122/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(i, a, b) for (int i = a; i < b; i++)
+#define ringr(i, a, b) for (int i = a; i > b; i--)
+#define rings(i, a, b, step) for (int i = a; i < b; i += step)
+#define sz(n) (n).size()
+#define ln cout << '\n'
+#define clr(mem, i) memset(mem, i, sizeof(mem))
+
+void solve() {
+  set<int> numbers = {4, 7, 44, 47, 74, 77 , 444, 447, 474, 477,
+  744, 747, 777};
+  int n ; cin >> n;
+  for(auto i : numbers){
+    if(n % i == 0){
+      cout << "YES\n"; return;
+    }
+  }
+  cout << "NO\n";
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t = 1;
+  // cin >> t;
+  while (t--) {
+    solve();
+  }
+}
