7b79b86958765fd3577cc5be3fb9da39f327e937
Ahmed Essam <ahmedessamjax@gmail.com>
2026-08-31 14:15:50 +0300

    add codeforces/String Task solution

---
+++ codeforces/String Task.cpp
@@ -0,0 +1,44 @@
+// problem name: String Task
+// problem link: https://codeforces.com/problemset/problem/118/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(i, a, b) for (int i = a; i < b; i++)
+#define ringr(i, a, b) for (int i = a; i > b; i--)
+#define rings(i, a, b, step) for (int i = a; i < b; i += step)
+#define sz(n) (n).size()
+#define ln cout << '\n'
+#define clr(mem, i) memset(mem, i, sizeof(mem))
+
+void solve() {
+  string s; cin >> s;
+  set<char> vowels = {'a', 'e', 'i', 'o', 'u', 'y'};
+  for(char c : s){
+    char lower = (char)tolower(c);
+    if(vowels.count(lower)) continue;
+    cout << '.' << lower; 
+  }
+  ln;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t = 1;
+  // cin >> t;
+  while (t--) {
+    solve();
+  }
+}
