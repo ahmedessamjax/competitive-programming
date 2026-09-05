a627de8eaa178afe210217cf7e8f0e7fbecbb72d
Ahmed Essam <ahmedessamjax@gmail.com>
2026-09-06 00:29:26 +0300

    add codeforces/Minus Two solution

---
+++ codeforces/Minus Two.cpp
@@ -0,0 +1,48 @@
+// problem name: Minus Two
+// problem link: https://codeforces.com/contest/2259/problem/B
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(i, a, b) for (int i = a; i < b; i++)
+#define ringr(i, a, b) for (int i = a; i > b; i--)
+#define rings(i, a, b, step) for (int i = a; i < b; i += step)
+#define sz(n) (n).size()
+#define ln cout << '\n'
+#define clr(mem, i) memset(mem, i, sizeof(mem))
+
+void solve() {
+  int n; cin >> n;
+  int odds = 0;
+  int evenOdds =0, evenEvens = 0;
+  while(n--){
+    int x; cin >> x;
+    if(x & 1) odds++;
+    else {
+      if((x / 2) & 1) evenOdds++;
+      else evenEvens++;
+    }
+  }
+  cout << max(max(evenOdds, evenEvens), odds) << '\n';
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
