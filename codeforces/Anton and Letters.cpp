d4506fd2d910d4ec577e189f0090471258937839
Ahmed Essam <ahmedessamjax@gmail.com>
2026-09-06 02:28:34 +0300

    add codeforces/Anton and Letters solution

---
+++ codeforces/Anton and Letters.cpp
@@ -0,0 +1,43 @@
+// problem name: Anton and Letters
+// problem link: https://codeforces.com/problemset/problem/443/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(i, a, b) for (int i = a; i < b; i++)
+#define ringr(i, a, b) for (int i = a; i > b; i--)
+#define rings(i, a, b, step) for (int i = a; i < b; i += step)
+#define sz(n) (n).size()
+#define ln cout << '\n'
+#define clr(mem, i) memset(mem, i, sizeof(mem))
+
+void solve() {
+  set<char> characters ;
+  string s; 
+  getline(cin, s);
+  ring(i,0,sz(s)){
+    if(s[i] >= 'a' and s[i] <= 'z') characters.insert(s[i]);
+  }
+  cout << sz(characters) << '\n';
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t = 1;
+  // cin >> t;
+  while (t--) {
+    solve();
+  }
+}
