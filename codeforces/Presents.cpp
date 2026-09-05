0da376f6d66ae16b489da9ccbd6cf6d956cc5848
Ahmed Essam <ahmedessamjax@gmail.com>
2026-09-06 02:15:54 +0300

    add codeforces/Presents solution

---
+++ codeforces/Presents.cpp
@@ -0,0 +1,45 @@
+// problem name: Presents
+// problem link: https://codeforces.com/problemset/problem/136/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(i, a, b) for (int i = a; i < b; i++)
+#define ringr(i, a, b) for (int i = a; i > b; i--)
+#define rings(i, a, b, step) for (int i = a; i < b; i += step)
+#define sz(n) (n).size()
+#define ln cout << '\n'
+#define clr(mem, i) memset(mem, i, sizeof(mem))
+
+void solve() {
+  int n ; cin >> n;
+  int arr[n];
+  ring(i,0,n) cin >> arr[i];
+  int ans[n];
+  ring(i,0,n){
+    ans[arr[i]-1] = i+1;  
+  }
+  ring(i,0,n) cout << ans[i] << " ";
+  ln;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t = 1;
+  // cin >> t;
+  while (t--) {
+    solve();
+  }
+}
