f150e23a7a5240257b1d8787625466d1a749ae9f
Ahmed Essam <ahmedessamjax@gmail.com>
2026-09-07 18:34:34 +0300

    add csec/Missing Number solution

---
+++ cses/Missing Number.cpp
@@ -0,0 +1,45 @@
+// problem name: Missing Number
+// problem link: https://cses.fi/problemset/task/1083
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(i, a, b) for (int i = a; i < b; i++)
+#define ringr(i, a, b) for (int i = a; i > b; i--)
+#define rings(i, a, b, step) for (int i = a; i < b; i += step)
+#define sz(n) (n).size()
+#define ln cout << '\n'
+#define clr(mem, i) memset(mem, i, sizeof(mem))
+
+void solve() {
+  ll n; cin >> n;
+  ll tmpN = n;
+  tmpN--;
+  ll sum = 0;
+  while(tmpN--){
+    ll x; cin >> x;
+    sum += x;
+  }
+  cout << (n * (n+1) / 2) - sum << '\n';
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t = 1;
+  // cin >> t;
+  while (t--) {
+    solve();
+  }
+}
