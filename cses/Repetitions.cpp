efccd376c0a23052dd1ef1fa00bdfc97d114bf91
Ahmed Essam <ahmedessamjax@gmail.com>
2026-09-07 18:37:04 +0300

    add csec/Repetitions solution

---
+++ cses/Repetitions.cpp
@@ -0,0 +1,50 @@
+// problem name: Repetitions
+// problem link: https://cses.fi/problemset/task/1069/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(i, a, b) for (int i = a; i < b; i++)
+#define ringr(i, a, b) for (int i = a; i > b; i--)
+#define rings(i, a, b, step) for (int i = a; i < b; i += step)
+#define sz(n) (n).size()
+#define ln cout << '\n'
+#define clr(mem, i) memset(mem, i, sizeof(mem))
+
+void solve() {
+  string s ; cin >> s;
+  int mx = -1;
+  int cnt = 0;
+  ring(i,0,sz(s)-1){
+    if(s[i] == s[i+1]){
+     cnt++; 
+    } 
+    else {
+      mx = max(cnt, mx);
+      cnt = 0;
+    }
+  }
+  mx = max(cnt,mx);
+  cout << mx+1 << '\n';
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t = 1;
+  // cin >> t;
+  while (t--) {
+    solve();
+  }
+}
