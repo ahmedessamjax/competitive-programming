152147361c643da3003ebe6eb0e5586ad7b46260
Ahmed Essam <ahmedessamjax@gmail.com>
2026-09-24 01:53:38 +0300

    add codeforces/Insomnia cure solution

---
+++ codeforces/Insomnia cure.cpp
@@ -0,0 +1,43 @@
+// problem name: Insomnia cure
+// problem link: https://codeforces.com/problemset/problem/148/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(i, a, b) for (int i = a; i < b; i++)
+#define ringr(i, a, b) for (int i = a; i > b; i--)
+#define rings(i, a, b, step) for (int i = a; i < b; i += step)
+#define sz(n) (n).size()
+#define ln cout << '\n'
+#define clr(mem, i) memset(mem, i, sizeof(mem))
+
+void solve() {
+  int k,l,m,n,d;
+  cin >> k >> l >> m >> n >> d;
+  int cnt = d;;
+  ring(i,1, d+1){
+    if(!(i % k == 0 || i % l == 0 || i % m == 0 || i % n == 0) ) cnt--;
+  }
+  cout << cnt << '\n';
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t = 1;
+  // cin >> t;
+  while (t--) {
+    solve();
+  }
+}
